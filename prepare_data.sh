76761f691d979eaf0741e111197a28d0ccab312f
Juan Lao Tebar <nitzing@gmail.com>
2018-01-06 01:08:20 +0100

    there is no need to execute obtain_word2vec.py

--- prepare_data.sh
+++ prepare_data.sh
@@ -12,4 +12,4 @@ wget "http://nlpserver2.inf.ufrgs.br/alexandres/vectors/lexvec.commoncrawl.300d.
 gzip -d lexvec.commoncrawl.300d.W.pos.vectors.gz
 
 echo 'Manually download GoogleNews-vectors-negative300.bin.gz in this directory from https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing'
-echo 'Then extract it and execute obtain_word2vec.py'
+echo 'Then extract it'
